5d0f0455c672c3421e513169151652b98591fe11
bennettftomlin <44040885+bennettftomlin@users.noreply.github.com>
2019-04-09 18:19:21 -0500

    Update server.py
    replaced model and classes

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&confirm=x4wY&id=1oau0kRw8G7jLhQ3Z2iYzI88LyaPCtrdu'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['s', '3', 'x']
 path = Path(__file__).parent
 
 app = Starlette()
